72790ad62ab0f75f12ea29b127e9e0a93d40b93d
yanlun0323 <yanlun0323@163.com>
2020-02-20 16:57:00 +0800

    新增测试案例

---
+++ src/main/java/java/lang/String.java
@@ -0,0 +1,12 @@
+package java.lang;
+
+/**
+ * @author yan
+ */
+public class String {
+
+    public static void main(String[] args) {
+        System.out.println("customize class String");
+    }
+
+}
