90369abd88409565e94860eb12f2042fb69837e5
yanlun0323 <yanlun0323@163.com>
2020-04-10 14:06:52 +0800

    UPT 提供两种获取Bean实例的方式：1、getBean(Class<T> type)根据getObject获取Bean；2、getBean("&实例对象名")获取Bean的自身实例

--- core/src/main/java/com/customized/libs/core/libs/spring/components/scanner/test/SpringBootComponentScanStarterTest.java
+++ core/src/main/java/com/customized/libs/core/libs/spring/components/scanner/test/SpringBootComponentScanStarterTest.java
@@ -1,23 +1,33 @@
 package com.customized.libs.core.libs.spring.components.scanner.test;
 
 import com.customized.libs.core.libs.spring.components.scanner.EnableMyComponentScan;
+import com.customized.libs.core.libs.spring.components.scanner.expand.MyBeanProxyFactory;
 import com.customized.libs.core.libs.spring.components.scanner.test.service.UserService;
 import com.customized.libs.core.libs.spring.components.scanner.test.service.UserSessionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Objects;
+
 /**
  * @author yan
  */
 @SpringBootApplication
 @EnableMyComponentScan(scanBasePackages = "com.customized.libs.core.libs.spring.components.scanner.test.service")
+@SuppressWarnings("unchecked")
 public class SpringBootComponentScanStarterTest {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringBootComponentScanStarterTest.class);
 
+        // 获取FactoryBean代理后的对象
         context.getBean(UserService.class).hello("Hello World!!");
+
+        // 获取FactoryBean实际对象
+        MyBeanProxyFactory<UserService> beanProxyFactory = (MyBeanProxyFactory) context.getBean("&userServiceImpl");
+        Objects.requireNonNull(beanProxyFactory.getObject()).hello("Hello World!!");
+
         context.getBean(UserSessionService.class).getUserSession();
     }
 }
