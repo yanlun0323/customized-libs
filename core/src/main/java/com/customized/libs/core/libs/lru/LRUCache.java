e52d88d9697ad9101a7c2e413b6ca931bb8476b8
yanlun0323 <yanlun0323@163.com>
2020-07-21 14:08:33 +0800

    利用JDK-LinkedHashMap实现LRU算法

--- core/src/main/java/com/customized/libs/core/libs/lru/LRUCache.java
+++ core/src/main/java/com/customized/libs/core/libs/lru/LRUCache.java
@@ -12,7 +12,7 @@ import java.util.Map;
 class LRUCache<K, V> extends LinkedHashMap<K, V> {
     private static final long serialVersionUID = -5935353900352027174L;
 
-    private final int CACHE_SIZE;
+    private final int cacheSize;
 
     /**
      * 传递进来最多能缓存多少数据
@@ -22,7 +22,7 @@ class LRUCache<K, V> extends LinkedHashMap<K, V> {
     public LRUCache(int cacheSize) {
         // true 表示让 linkedHashMap 按照访问顺序来进行排序，最近访问的放在头部，最老访问的放在尾部。
         super((int) Math.ceil(cacheSize / 0.75) + 1, 0.75f, true);
-        CACHE_SIZE = cacheSize;
+        this.cacheSize = cacheSize;
     }
 
     /**
@@ -32,6 +32,6 @@ class LRUCache<K, V> extends LinkedHashMap<K, V> {
     @Override
     protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
         // 当 map中的数据量大于指定的缓存个数的时候，就自动删除最老的数据。
-        return size() > CACHE_SIZE;
+        return size() > cacheSize;
     }
 }
