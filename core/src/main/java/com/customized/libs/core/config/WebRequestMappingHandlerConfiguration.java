7c778dc626ec7a9cf0aad26f1658df815304b5e5
yanlun0323 <yanlun0323@163.com>
2020-09-02 08:45:02 +0800

    features: Spring WebMvc Multi Version Handler Components

--- core/src/main/java/com/customized/libs/core/config/WebRequestMappingHandlerConfiguration.java
+++ core/src/main/java/com/customized/libs/core/config/WebRequestMappingHandlerConfiguration.java
@@ -12,11 +12,26 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 @Configuration
 public class WebRequestMappingHandlerConfiguration extends WebMvcConfigurationSupport {
 
+    /**
+     * 重写RequestMappingHandlerMapping，支持API多版本处理
+     *
+     * @return
+     */
     @Override
     public RequestMappingHandlerMapping requestMappingHandlerMapping() {
-        MultiVersionRequestMappingHandlerMapping requestMappingHandlerMapping = new MultiVersionRequestMappingHandlerMapping();
-        requestMappingHandlerMapping.registerApiVersionCodeDiscoverer(new DefaultApiVersionCodeDiscoverer());
-        return requestMappingHandlerMapping;
+        return super.requestMappingHandlerMapping();
+    }
 
+    /**
+     * Protected method for plugging in a custom subclass of
+     * {@link RequestMappingHandlerMapping}.
+     *
+     * @since 4.0
+     */
+    @Override
+    protected RequestMappingHandlerMapping createRequestMappingHandlerMapping() {
+        MultiVersionRequestMappingHandlerMapping handlerMapping = new MultiVersionRequestMappingHandlerMapping();
+        handlerMapping.registerApiVersionCodeDiscoverer(new DefaultApiVersionCodeDiscoverer());
+        return handlerMapping;
     }
 }
