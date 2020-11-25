40b1c5e6482be22062034e5172ede43915291531
yanlun0323 <yanlun0323@163.com>
2020-11-25 15:18:18 +0800

    spring-context-indexer 预扫描导致了Swagger2项目出现故障，无法访问

--- springboot/src/main/java/com/customized/libs/springboot/config/WebMvcConfig.java
+++ springboot/src/main/java/com/customized/libs/springboot/config/WebMvcConfig.java
@@ -17,6 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
+
         // 解决 SWAGGER 404报错
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
